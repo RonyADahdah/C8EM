125ecead7a8c92ccf4b83b6b6d0bbdbc9830e9f9
RonyADahdah <ronny.dahdah.986@gmail.com>
2026-05-16 11:04:06 +0300

    Initial commit

---
+++ cpu.c
@@ -0,0 +1,5 @@
+//
+// Created by Rony-PC on 5/16/2026.
+//
+
+#include "cpu.h"
---
+++ cpu.h
@@ -0,0 +1,8 @@
+//
+// Created by Rony-PC on 5/16/2026.
+//
+
+#ifndef C8EM_CPU_H
+#define C8EM_CPU_H
+
+#endif //C8EM_CPU_H
